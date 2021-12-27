796a51f1906b608215c0efee5dd526a3fa071a04
VizeModz <96720993+VizeModz@users.noreply.github.com>
2021-12-27 21:05:31 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,13 @@
---Settings
+--Libs 
+local uiLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/VizeModz/ploitHUB/main/uiLib.lua"))()
+local notifyLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/VizeModz/ploitHUB/main/notifyLib.lua"))()
+
+--Util
 local Config = getgenv().settings
+local Notify = notifyLib.init
 
 Config = {
    ['Label'] = 'ploitHUB',
+   ['MenuIcon'] = 8376928790,
+   ['ItemIcon'] = 1604601556
 }
